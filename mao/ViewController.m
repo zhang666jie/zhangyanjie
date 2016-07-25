b1af75abd79fcf26fece21ba21444d994617c5fe
zhang666jie <904980238@qq.com>
2016-07-25 14:46:43 +0800

    904980238@qq.com
    allusive

---
+++ mao/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  mao
+//
+//  Created by Mac on 16/7/25.
+//  Copyright © 2016年 ZYJ. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
